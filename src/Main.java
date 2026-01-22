7a24039ab959a0898c29fe82a5bc5acecd116b57
Raquel Monteiro <islanmir@hotmail.com>
2026-01-22 19:53:47 +0000

    desafio feito com sucesso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
-package br.com.dio.desafio.dominio;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição Curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        // Dev Ana
+        Dev ana = new Dev();
+        ana.setNome("Ana");
+        ana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ana: " + ana.getConteudosInscritos());
+        ana.progredir();
+        ana.progredir();
+        System.out.println("Conteúdos Inscritos Ana: " + ana.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ana: " + ana.getConteudosConcluidos());
+        System.out.println("XP: " + ana.calcularTotalXp());
+
+        System.out.println("-------");
+
+        // Dev Pedro
+        Dev pedro = new Dev();
+        pedro.setNome("Pedro");
+        pedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Pedro: " + pedro.getConteudosInscritos());
+        pedro.progredir();
+        pedro.progredir();
+        pedro.progredir();
+        System.out.println("Conteúdos Inscritos Pedro: " + pedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Pedro: " + pedro.getConteudosConcluidos());
+        System.out.println("XP: " + pedro.calcularTotalXp());
+    }
 }
+
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,5 +1,29 @@
 package br.com.dio.desafio.dominio;
 
-public class Conteudo {
-    
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descricao;
+
+    // Força subclasses a implementar cálculo de XP
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
+
